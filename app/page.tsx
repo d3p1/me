aec03e22bee3991bcf0cfd7e7100b26c381ac38c
C. M. de Picciotto <d3p1@d3p1.dev>
2025-02-22 01:00:06 +0000

    feat: add blur effect to wip banner [#1]

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,12 @@
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
 import Image from 'next/image'
+import wipBanner from '@/public/media/banners/wip.jpg'
 
 export default function Home() {
   return (
     <main className="flex justify-center items-center w-screen h-screen">
-      <Image
-        src="/media/banners/wip.jpg"
-        alt="Work in progress"
-        width={400}
-        height={400}
-      />
+      <Image src={wipBanner} alt="Work in progress" placeholder="blur" />
     </main>
   )
 }
