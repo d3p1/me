883f15986b84c44b852d85923698d4fb160c9dcb
C. M. de Picciotto <d3p1@d3p1.dev>
2026-01-06 20:47:25 +0000

    feat: init profile image pixel effect [#1]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,49 @@
  * @description Root page
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
-import Image from 'next/image'
-import profile from '../../public/media/home/profile.png'
+'use client'
+
+import {Img2Pxl} from '@d3p1/img2pxl/react'
+
+const config = {
+  images: {
+    0: {
+      src: 'media/home/profile.png',
+      width: 280,
+      height: 280,
+      resolution: {
+        width: 70,
+        height: 70,
+      },
+      pixel: {
+        size: 3,
+        motion: {
+          displacement: {
+            frequency: 1,
+            amplitude: 40,
+          },
+        },
+      },
+      motion: {
+        noise: {
+          frequency: 0.05,
+          amplitude: 2,
+        },
+      },
+    },
+  },
+  pointer: {
+    size: 0.1,
+    trailing: {
+      factor: 0.01,
+    },
+  },
+}
 
 export default function HomePage() {
   return (
     <main className="flex justify-center items-center w-screen h-screen">
-      <Image
-        src={profile}
-        alt="Cristian Marcelo de Picciotto profile image"
-        placeholder="blur"
-      />
+      <Img2Pxl {...config} />
     </main>
   )
 }
