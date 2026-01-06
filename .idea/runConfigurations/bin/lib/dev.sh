5b39a4b5169efa13408a887d9cbcf2d5b0af0428
C. M. de Picciotto <d3p1@d3p1.dev>
2026-01-05 22:24:15 -0300

    feat: upgrade platform version [#1]

--- .idea/runConfigurations/bin/lib/dev.sh
+++ .idea/runConfigurations/bin/lib/dev.sh
@@ -11,7 +11,27 @@
 # @return void
 ##
 main() {
-    docker compose run --rm -it -v ~/.ssh/:/home/node/.ssh/ -v ~/.gitconfig:/home/node/.gitconfig cli /bin/bash
+    ##
+    # @note Because this environment could have different users
+    #       depending on the JavaScript runtime used (for instance:
+    #       the `bun` image uses the `bun` user, whereas the `node` image
+    #       uses the `node` user), we are going to get the container
+    #       home directory dynamically to later mount important files
+    #       that improve development experience there
+    ##
+    local CONTAINER_HOME_DIR=$(docker compose run --rm cli bash -c 'echo "$HOME"')
+
+    ##
+    # @note It is shared Git configuration (like author information),
+    #       to avoid having to define it again in the container
+    #       while using Git
+    # @note It is mounted the SSH agent socket (`$SSH_AUTH_SOCK`)
+    #       to the container. In that way, we can use it to
+    #       execute SSH connections to our trusted hosts
+    #       without sharing our private keys.
+    #       This is useful to work with SSH repositories
+    ##
+    docker compose run --rm -it -v ~/.gitconfig:${CONTAINER_HOME_DIR}/.gitconfig -v ${SSH_AUTH_SOCK}:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent cli /bin/bash
 }
 
 ##
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
 import Image from 'next/image'
-import wipBanner from '@/public/media/banners/wip.jpg'
+import wipBanner from '../../public/media/banners/wip.jpg'
 
 export default function Home() {
   return (
