e5fb74e7fcea96309c27d90bc52751bc8618c70b
C. M. de Picciotto <d3p1@d3p1.dev>
2026-01-06 20:37:40 +0000

    feat: add profile image to home page [#1]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,16 @@
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
 import Image from 'next/image'
-import wipBanner from '../../public/media/banners/wip.jpg'
+import profile from '../../public/media/home/profile.png'
 
-export default function Home() {
+export default function HomePage() {
   return (
     <main className="flex justify-center items-center w-screen h-screen">
-      <Image src={wipBanner} alt="Work in progress" placeholder="blur" />
+      <Image
+        src={profile}
+        alt="Cristian Marcelo de Picciotto profile image"
+        placeholder="blur"
+      />
     </main>
   )
 }
