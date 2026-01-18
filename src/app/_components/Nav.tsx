208913367a407619f8d78ab46bfec73c5e76e07c
C. M. de Picciotto <d3p1@d3p1.dev>
2026-01-18 16:15:13 +0000

    refactor: improve nav component [#1]

--- src/app/_components/Nav.tsx
+++ src/app/_components/Nav.tsx
@@ -2,67 +2,22 @@
  * @description Nav component
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
+import {links} from '@/app/_etc/nav'
+
 export default function Nav() {
   return (
-    <nav>
-      <ul className="grid grid-cols-3 gap-4">
-        <li className="opacity-50 hover:opacity-100 transition-opacity">
-          <a
-            className="before:content-['_[_'] after:content-['_]_']"
-            href="https://www.linkedin.com/in/cristian-marcelo-de-picciotto/"
-          >
-            linkedin
-          </a>
-        </li>
-        <li className="opacity-50 hover:opacity-100 transition-opacity">
-          <a
-            className="before:content-['_[_'] after:content-['_]_']"
-            href="https://www.upwork.com/freelancers/cristianmarcelodepicciotto"
-          >
-            upwork
-          </a>
-        </li>
-        <li className="opacity-50 hover:opacity-100 transition-opacity">
-          <a
-            className="before:content-['_[_'] after:content-['_]_']"
-            href="https://github.com/d3p1"
-          >
-            github
-          </a>
-        </li>
-        <li className="opacity-50 hover:opacity-100 transition-opacity">
-          <a
-            className="before:content-['_[_'] after:content-['_]_']"
-            href="https://www.chess.com/member/depichess"
-          >
-            chess.com
-          </a>
-        </li>
-        <li className="opacity-50 hover:opacity-100 transition-opacity">
-          <a
-            className="before:content-['_[_'] after:content-['_]_']"
-            href="https://lichess.org/@/depichess"
-          >
-            lichess
-          </a>
-        </li>
-        <li className="opacity-50 hover:opacity-100 transition-opacity">
-          <a
-            className="before:content-['_[_'] after:content-['_]_']"
-            href="https://x.com/___d3p1"
-          >
-            twitter
-          </a>
-        </li>
-        <li className="opacity-50 hover:opacity-100 transition-opacity">
-          <a
-            className="before:content-['_[_'] after:content-['_]_']"
-            href="https://www.instagram.com/___d3p1"
-          >
-            instagram
-          </a>
-        </li>
-      </ul>
+    <nav aria-label="Social links" className="grid grid-cols-3 gap-4">
+      {links.map(({label, href}, index) => (
+        <a
+          key={index}
+          className="before:content-['_[_'] after:content-['_]_'] opacity-50 hover:opacity-100 transition-opacity"
+          href={href}
+          target="_blank"
+          rel="me"
+        >
+          {label}
+        </a>
+      ))}
     </nav>
   )
 }
---
+++ src/app/_etc/nav.ts
@@ -0,0 +1,19 @@
+/**
+ * @description Nav config
+ * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
+ */
+export const links = [
+  {
+    label: 'linkedin',
+    href: 'https://www.linkedin.com/in/cristian-marcelo-de-picciotto/',
+  },
+  {
+    label: 'upwork',
+    href: 'https://www.upwork.com/freelancers/cristianmarcelodepicciotto',
+  },
+  {label: 'github', href: 'https://github.com/d3p1'},
+  {label: 'chess.com', href: 'https://www.chess.com/member/depichess'},
+  {label: 'lichess', href: 'https://lichess.org/@/depichess'},
+  {label: 'twitter/x', href: 'https://x.com/___d3p1'},
+  {label: 'instagram', href: 'https://www.instagram.com/___d3p1'},
+]
