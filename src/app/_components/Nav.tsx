0cbd43c409d762831b66c5be65221bd79883517d
C. M. de Picciotto <d3p1@d3p1.dev>
2026-01-18 15:54:20 +0000

    feat: add nav component [#1]

--- src/app/_components/Nav.tsx
+++ src/app/_components/Nav.tsx
@@ -1,35 +1,66 @@
 /**
- * @description Nav page
+ * @description Nav component
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
 export default function Nav() {
   return (
     <nav>
-      <ul>
-        <li>
-          <a href="https://www.linkedin.com/in/cristian-marcelo-de-picciotto/">
+      <ul className="grid grid-cols-3 gap-4">
+        <li className="opacity-50 hover:opacity-100 transition-opacity">
+          <a
+            className="before:content-['_[_'] after:content-['_]_']"
+            href="https://www.linkedin.com/in/cristian-marcelo-de-picciotto/"
+          >
             linkedin
           </a>
         </li>
-        <li>
-          <a href="https://www.upwork.com/freelancers/cristianmarcelodepicciotto">
+        <li className="opacity-50 hover:opacity-100 transition-opacity">
+          <a
+            className="before:content-['_[_'] after:content-['_]_']"
+            href="https://www.upwork.com/freelancers/cristianmarcelodepicciotto"
+          >
             upwork
           </a>
         </li>
-        <li>
-          <a href="https://github.com/d3p1">github</a>
+        <li className="opacity-50 hover:opacity-100 transition-opacity">
+          <a
+            className="before:content-['_[_'] after:content-['_]_']"
+            href="https://github.com/d3p1"
+          >
+            github
+          </a>
         </li>
-        <li>
-          <a href="https://www.chess.com/member/depichess">chess.com</a>
+        <li className="opacity-50 hover:opacity-100 transition-opacity">
+          <a
+            className="before:content-['_[_'] after:content-['_]_']"
+            href="https://www.chess.com/member/depichess"
+          >
+            chess.com
+          </a>
         </li>
-        <li>
-          <a href="https://lichess.org/@/depichess">lichess</a>
+        <li className="opacity-50 hover:opacity-100 transition-opacity">
+          <a
+            className="before:content-['_[_'] after:content-['_]_']"
+            href="https://lichess.org/@/depichess"
+          >
+            lichess
+          </a>
         </li>
-        <li>
-          <a href="https://x.com/___d3p1">twitter</a>
+        <li className="opacity-50 hover:opacity-100 transition-opacity">
+          <a
+            className="before:content-['_[_'] after:content-['_]_']"
+            href="https://x.com/___d3p1"
+          >
+            twitter
+          </a>
         </li>
-        <li>
-          <a href="https://www.instagram.com/___d3p1">instagram</a>
+        <li className="opacity-50 hover:opacity-100 transition-opacity">
+          <a
+            className="before:content-['_[_'] after:content-['_]_']"
+            href="https://www.instagram.com/___d3p1"
+          >
+            instagram
+          </a>
         </li>
       </ul>
     </nav>
