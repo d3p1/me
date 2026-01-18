1df3aa0f71375f5cacea4e0bce4c0d89065fc073
C. M. de Picciotto <d3p1@d3p1.dev>
2026-01-18 00:15:13 +0000

    feat: add navigation links [#1]

---
+++ src/app/_components/Nav.tsx
@@ -0,0 +1,37 @@
+/**
+ * @description Nav page
+ * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
+ */
+export default function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="https://www.linkedin.com/in/cristian-marcelo-de-picciotto/">
+            linkedin
+          </a>
+        </li>
+        <li>
+          <a href="https://www.upwork.com/freelancers/cristianmarcelodepicciotto">
+            upwork
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/d3p1">github</a>
+        </li>
+        <li>
+          <a href="https://www.chess.com/member/depichess">chess.com</a>
+        </li>
+        <li>
+          <a href="https://lichess.org/@/depichess">lichess</a>
+        </li>
+        <li>
+          <a href="https://x.com/___d3p1">twitter</a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/___d3p1">instagram</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
