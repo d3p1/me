eb74242f135b042c2f4c76f89557246be518f3ac
C. M. de Picciotto <d3p1@d3p1.dev>
2026-01-19 23:24:31 +0000

    feat: add not found page [#1]

---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+/**
+ * @description Not found page
+ * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
+ */
+export default function NotFoundPage() {
+  return <h1 className="before:content-['['] after:content-[']']">Not Found</h1>
+}
