a9210155a72536545d7cee25be8496232c0d30b4
C. M. de Picciotto <d3p1@d3p1.dev>
2026-01-20 00:57:09 +0000

    refactor: upgrade img2pxl library effect [#1]

--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -2,8 +2,6 @@
  * @description Hero component
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
-'use client'
-
 import {Img2Pxl} from '@d3p1/img2pxl/react'
 import {config} from '@/app/_etc/hero'
 
