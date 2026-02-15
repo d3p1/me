7421a056b29a23b024d831289d55165d6af6bcaa
C. M. de Picciotto <d3p1@d3p1.dev>
2026-02-15 19:12:45 +0000

    feat: add github config [#1]

---
+++ src/app/_etc/github.ts
@@ -0,0 +1,13 @@
+/**
+ * @description GitHub config
+ * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
+ */
+export const LABORATORY_TOPIC = 'laboratory'
+
+export const PROJECT_TOPIC = 'project'
+
+export const SEARCH_STARS_SORT = 'stars'
+
+export const SEARCH_STARS_SORT_ORDER = 'desc'
+
+export const SEARCH_PAGE_SIZE = 100
