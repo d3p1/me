f277c2d30e49cd80b01e2c1920cacf06fb43c72c
C. M. de Picciotto <d3p1@d3p1.dev>
2026-02-15 19:12:11 +0000

    feat: add lab type [#1]

---
+++ src/app/_types/index.ts
@@ -0,0 +1,10 @@
+/**
+ * @description Types
+ * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
+ */
+export interface Lab {
+  full_name: string
+  html_url: string
+  description: string | null
+  topics: string[] | undefined
+}
