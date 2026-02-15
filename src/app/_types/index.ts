a65a02b8919829c3e35106ff0e80ae24b8869eb4
C. M. de Picciotto <d3p1@d3p1.dev>
2026-02-15 22:11:15 +0000

    fix: change lab type by repo type [#1]

--- src/app/_types/index.ts
+++ src/app/_types/index.ts
@@ -2,7 +2,8 @@
  * @description Types
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
-export interface Lab {
+export interface Repo {
+  id: string | number
   full_name: string
   html_url: string
   description: string | null
