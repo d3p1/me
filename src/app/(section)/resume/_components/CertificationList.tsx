9d6eb59860be08f54fde4c7ce2d8381ed009c107
C. M. de Picciotto <d3p1@d3p1.dev>
2026-02-23 19:02:23 +0000

    feat: init resume certification list component [#1]

---
+++ src/app/(section)/resume/_components/CertificationList.tsx
@@ -0,0 +1,26 @@
+/**
+ * @description Certification list
+ * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
+ */
+import List from '@/app/(section)/resume/_components/List'
+import Item from '@/app/(section)/resume/_components/List/Item'
+import {certifications} from '@/app/(section)/resume/_etc/certifications'
+
+export default function CertificationList() {
+  return (
+    <List
+      title="Certifications"
+      items={certifications.map((certification, index) => (
+        <Item li={certification} key={index}>
+          <a
+            href={certification.provider.link}
+            target="_blank"
+            className="text-secondary-500 font-black"
+          >
+            Provider: {certification.provider.name}
+          </a>
+        </Item>
+      ))}
+    />
+  )
+}
