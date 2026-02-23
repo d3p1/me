6ed558723acb7065a84075f9a1a020d5f15faf42
C. M. de Picciotto <d3p1@d3p1.dev>
2026-02-23 19:24:29 +0000

    feat: add resume page [#1]

--- src/app/(section)/resume/page.tsx
+++ src/app/(section)/resume/page.tsx
@@ -2,26 +2,32 @@
  * @description Resume
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
-import {experiences} from '@/app/(section)/resume/_etc/experiences'
-import {projects} from '@/app/(section)/resume/_etc/projects'
-import {certifications} from '@/app/(section)/resume/_etc/certifications'
+import ProjectList from '@/app/(section)/resume/_components/ProjectList'
+import ExperienceList from '@/app/(section)/resume/_components/ExperienceList'
+import CertificationList from '@/app/(section)/resume/_components/CertificationList'
 
 export default function Page() {
   return (
-    <>
-      <h1>de Picciotto, Cristian Marcelo</h1>
+    <div className="flex flex-col gap-6">
+      <div className="flex flex-col items-center justify-center gap-6 text-center mb-8">
+        <h1>de Picciotto, Cristian Marcelo</h1>
 
-      <p>
-        Adobe Magento Certified Professional Developer | Next.js Developer |
-        Three.js Developer
-      </p>
+        <h2 className="text-secondary-500">
+          Adobe Magento Certified Expert Developer | Next.js Developer |
+          Three.js Developer
+        </h2>
 
-      <p>
-        Magento Certified Professional Developer with 13+ years of hands-on
-        experience designing, building, and optimizing complex eCommerce
-        platforms. Beyond eCommerce, I have a strong passion for DevOps,
-        graphics programming, and game development.
-      </p>
-    </>
+        <p>
+          Adobe Magento Certified Expert Developer with 13+ years of hands-on
+          experience designing, building, and optimizing complex eCommerce
+          platforms. Beyond eCommerce, I have a strong passion for DevOps,
+          graphics programming, and game development.
+        </p>
+      </div>
+
+      <ProjectList />
+      <ExperienceList />
+      <CertificationList />
+    </div>
   )
 }
