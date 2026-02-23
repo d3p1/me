c7633e2f874eafd11ba9ef82b90032f2ca25a32d
C. M. de Picciotto <d3p1@d3p1.dev>
2026-02-23 18:47:02 +0000

    feat: improve list component styles [#1]

--- src/app/(section)/resume/_components/List.tsx
+++ src/app/(section)/resume/_components/List.tsx
@@ -8,9 +8,7 @@ export default function List({title, items}: ListComponentProps) {
   return (
     <div className="pt-4 border-t-solid border-t-2 border-secondary-500">
       <h3 className="text-center text-secondary-500">{title}</h3>
-      <ul className="flex flex-col items-center justify-center gap-4">
-        {items}
-      </ul>
+      <ul className="flex flex-col gap-14">{items}</ul>
     </div>
   )
 }
