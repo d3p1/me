53c93ccd0fab9aa8941a5a19865d8b190892963e
C. M. de Picciotto <d3p1@d3p1.dev>
2026-02-23 18:18:51 +0000

    feat: init resume list component [#1]

---
+++ src/app/(section)/resume/_components/List.tsx
@@ -0,0 +1,16 @@
+/**
+ * @description List component
+ * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
+ */
+import type {ListComponentProps} from '@/app/(section)/resume/_types'
+
+export default function List({title, items}: ListComponentProps) {
+  return (
+    <div className="pt-4 border-t-solid border-t-2 border-secondary-500">
+      <h3 className="text-center text-secondary-500">{title}</h3>
+      <ul className="flex flex-col items-center justify-center gap-4">
+        {items}
+      </ul>
+    </div>
+  )
+}
