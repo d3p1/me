c8395c1b6c532692540dea8c0136624378ef755d
C. M. de Picciotto <d3p1@d3p1.dev>
2026-02-23 18:17:07 +0000

    feat: add types for resume components  [#1]

--- src/app/(section)/resume/_types/index.ts
+++ src/app/(section)/resume/_types/index.ts
@@ -2,9 +2,21 @@
  * @description Types
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
+import type {ReactElement, ReactNode} from 'react'
+
 export interface ListItem {
   title: string
   description?: string
+}
+
+export type ListItemComponentProps = {
+  li: ListItem
+  children?: ReactNode
+}
+
+export type ListComponentProps = {
+  title: string
+  items: ReactElement<ListItemComponentProps>[]
 }
 
 export interface Experience extends ListItem {
