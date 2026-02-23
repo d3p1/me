16c8e8970a3a767b37851b0dbfa9e8835c98141d
C. M. de Picciotto <d3p1@d3p1.dev>
2026-02-23 22:48:55 +0000

    feat: add internal and external links to home page [#1]

--- src/app/_components/Nav.tsx
+++ src/app/_components/Nav.tsx
@@ -3,6 +3,8 @@
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
 import {links} from '@/app/_etc/nav'
+import Link from 'next/link'
+import {UrlObject} from 'node:url'
 
 export default function Nav() {
   return (
@@ -10,17 +12,29 @@ export default function Nav() {
       aria-label="Social links"
       className="grid grid-cols-1 gap-2 sm:grid-cols-2 sm:gap-4 lg:grid-cols-3"
     >
-      {links.map(({label, href}, index) => (
-        <a
-          key={index}
-          className="token opacity-50 hover:opacity-100 transition-opacity"
-          href={href}
-          target="_blank"
-          rel="me"
-        >
-          {label}
-        </a>
-      ))}
+      {links.map(({label, href, external}, index) =>
+        external ? (
+          <a
+            key={index}
+            className="token opacity-50 hover:opacity-100 transition-opacity"
+            href={href}
+            target="_blank"
+            rel="me"
+          >
+            {label}
+          </a>
+        ) : (
+          <Link
+            key={index}
+            className="token opacity-50 hover:opacity-100 transition-opacity"
+            href={href as unknown as UrlObject}
+            target="_blank"
+            rel="me"
+          >
+            {label}
+          </Link>
+        ),
+      )}
     </nav>
   )
 }
