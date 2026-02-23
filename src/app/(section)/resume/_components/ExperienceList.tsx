56f4479e63549f42ec0e937e5623793bdacb67f4
C. M. de Picciotto <d3p1@d3p1.dev>
2026-02-23 19:23:02 +0000

    feat: init resume experience list component [#1]

---
+++ src/app/(section)/resume/_components/ExperienceList.tsx
@@ -0,0 +1,30 @@
+/**
+ * @description Experience list
+ * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
+ */
+import List from '@/app/(section)/resume/_components/List'
+import Item from '@/app/(section)/resume/_components/List/Item'
+import {experiences} from '@/app/(section)/resume/_etc/experiences'
+
+export default function ExperienceList() {
+  const dateFormatter = new Intl.DateTimeFormat(undefined, {
+    month: 'long',
+    year: 'numeric',
+  })
+
+  return (
+    <List
+      title="Experience"
+      items={experiences.map((experience, index) => (
+        <Item li={experience} key={index}>
+          <div className="font-black text-secondary-500 flex flex-row gap-4">
+            <p>From: {dateFormatter.format(experience.start_date)}</p>
+            {experience.end_date && (
+              <p>To: {dateFormatter.format(experience.end_date)}</p>
+            )}
+          </div>
+        </Item>
+      ))}
+    />
+  )
+}
