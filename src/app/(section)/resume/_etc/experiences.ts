0b1bc09211bcf428a467c5a3a5ecb24896ebf8f6
C. M. de Picciotto <d3p1@d3p1.dev>
2026-03-07 21:10:46 +0000

    fix: improve experience description [#1]

--- src/app/(section)/resume/_etc/experiences.ts
+++ src/app/(section)/resume/_etc/experiences.ts
@@ -10,16 +10,16 @@ export const experiences: Experience[] = [
       'Magento Developer | Next.js Developer | Three.js Developer | Upwork Freelance',
     start_date: new Date('2016-05-01'),
     description:
-      'Freelancing allows me to leverage my knowledge across the full software development lifecycle — from architecture and development to deployment and production support — taking full end-to-end ownership of projects.',
+      'Freelancing allows me to leverage my knowledge across the full software development lifecycle—from architecture and development to deployment and production support—while taking full end-to-end ownership of projects. It also enables me to apply and further develop my frontend, backend, DevOps, and System Administration skills using technologies such as Magento, Docker, Next.js, React, Tailwind CSS, Three.js, and Blender.',
   },
   {
-    title: 'Magento Developer | Sepoy',
+    title: 'Magento Full-Stack Developer | Sepoy',
     start_date: new Date('2024-01-01'),
     description:
       'Working as a Magento full-stack developer, implementing modern websites using Hyvä, ScandiPWA, and other modern headless architectures.',
   },
   {
-    title: 'Magento Developer | Bina Commerce',
+    title: 'Magento Full-Stack Developer | Bina Commerce',
     start_date: new Date('2021-01-01'),
     end_date: new Date('2026-01-01'),
     description:
@@ -27,14 +27,14 @@ export const experiences: Experience[] = [
   },
   {
     title:
-      'Magento Developer | Scala Developer | Play Framework Developer | Bina Chess',
+      'Magento Full-Stack Developer | Scala Full-Stack Developer | Bina Chess',
     start_date: new Date('2021-01-01'),
     end_date: new Date('2026-01-01'),
     description:
       'Independent personal project. Bina Chess was an online chess platform where users verified their identity and participated in tournaments with monetary prizes. Built a custom Magento module enabling users to purchase tournament tickets directly from the product detail page (PDP). Developed the gameplay platform using Scala and the Play Framework. Designed and implemented a custom Single Sign-On (SSO) system to synchronize authentication and session state between the Magento store and the chess platform. One of my most valuable technical and entrepreneurial learning experiences.',
   },
   {
-    title: 'Magento Developer | Balloon Group',
+    title: 'Magento Full-Stack Developer | Balloon Group',
     start_date: new Date('2023-10-01'),
     end_date: new Date('2024-03-01'),
     description:
@@ -58,7 +58,7 @@ export const experiences: Experience[] = [
     hide: true,
   },
   {
-    title: 'Magento Developer | Frávega',
+    title: 'Magento Full-Stack Developer | Frávega',
     start_date: new Date('2014-01-01'),
     end_date: new Date('2016-01-01'),
     description:
